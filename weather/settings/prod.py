1216df7140f8d4109f310b2fc40eaf37792b2792
eunsung-git <silvery0224@naver.com>
2020-07-03 15:53:13 +0900

    Add AWS S3 settings

--- weather/settings/prod.py
+++ weather/settings/prod.py
@@ -10,3 +10,14 @@ ALLOWED_HOSTS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# S3
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = 'clothes'
+AWS_S3_REGION_NAME = 'ap-northeast-2' # 서울
+AWS_S3_ADDRESSING_STYLE = 'virtual'
+AWS_DEFAULT_ACL = None
