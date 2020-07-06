7a5d4d176cbd1f041619b61634030a5eff0056e4
eunsung-git <silvery0224@naver.com>
2020-07-06 09:11:18 +0900

    Fix bug

--- pages/recommendation.py
+++ pages/recommendation.py
@@ -30,68 +30,68 @@ def recommendation(temp, gender, closet):
     # 날씨에 따른 옷 선택지
     if weather_category[:4] == 'warm':
         if weather_category == 'warm1':
-            clothes_kind = {'상의':['민소매', '반팔'],
-                            '하의':['반바지', '치마'],
-                            '원피스':['원피스'],
-                            '신발':['운동화', '샌들', '구두']}
+            clothes_kind = {'top':['민소매', '반팔'],
+                            'bottom':['반바지', '치마'],
+                            'one':['원피스'],
+                            'shoes':['운동화', '샌들', '구두']}
         elif weather_category == 'warm2':
-            clothes_kind = {'상의':['반팔'], 
-                            '하의':['반바지', '면바지', '치마'],
-                            '신발':['운동화', '워커']}
+            clothes_kind = {'top':['반팔'], 
+                            'bottom':['반바지', '면바지', '치마'],
+                            'shoes':['운동화', '워커']}
         elif weather_category == 'warm3':
-            clothes_kind = {'상의':['긴팔', '후드티', '맨투맨'],
-                            '하의':['면바지', '슬렉스', '청바지', '치마'],
-                            '신발':['운동화', '워커']}
+            clothes_kind = {'top':['긴팔', '후드티', '맨투맨'],
+                            'bottom':['면바지', '슬렉스', '청바지', '치마'],
+                            'shoes':['운동화', '워커']}
         else:
-            clothes_kind = {'상의':['긴팔', '니트', '후드티', '맨투맨'],
-                            '하의':['면바지', '청바지', '슬렉스', '치마'],
-                            '원피스':['원피스'],
-                            '신발':['운동화', '워커']}
+            clothes_kind = {'top':['긴팔', '니트', '후드티', '맨투맨'],
+                            'bottom':['면바지', '청바지', '슬렉스', '치마'],
+                            'one':['원피스'],
+                            'shoes':['운동화', '워커']}
     else:
         if weather_category == 'cool1':
-            clothes_kind = {'상의':['셔츠', '긴팔'],
-                            '하의':['면바지', '청바지', '슬렉스', '치마'],
-                            '신발':['운동화', '워커'],
-                            '아우터':['자켓', '가디건']}
+            clothes_kind = {'top':['셔츠', '긴팔'],
+                            'bottom':['면바지', '청바지', '슬렉스', '치마'],
+                            'shoes':['운동화', '워커'],
+                            'outer':['자켓', '가디건']}
         elif weather_category == 'cool2':
-            clothes_kind = {'상의':['셔츠', '긴팔', '니트'],
-                            '하의':['청바지', '치마'],
-                            '신발':['운동화', '워커'], 
-                            '아우터':['자켓', '집업']}
+            clothes_kind = {'top':['셔츠', '긴팔', '니트'],
+                            'bottom':['청바지', '치마'],
+                            'shoes':['운동화', '워커'], 
+                            'outer':['자켓', '집업']}
         elif weather_category == 'cool3':
-            clothes_kind = {'상의':['셔츠', '긴팔', '니트'],
-                            '하의':['면바지', '청바지', '슬렉스', '치마'],
-                            '신발':['운동화', '부츠', '워커'], 
-                            '아우터':['코트', '가죽자켓']}
+            clothes_kind = {'top':['셔츠', '긴팔', '니트'],
+                            'bottom':['면바지', '청바지', '슬렉스', '치마'],
+                            'shoes':['운동화', '부츠', '워커'], 
+                            'outer':['코트', '가죽자켓']}
         else:
-            clothes_kind = {'상의':['셔츠', '긴팔', '니트'],
-                            '하의':['면바지', '청바지', '슬렉스', '치마'],
-                            '신발':['운동화', '부츠'],
-                            '아우터':['코트', '패딩']}
+            clothes_kind = {'top':['셔츠', '긴팔', '니트'],
+                            'bottom':['면바지', '청바지', '슬렉스', '치마'],
+                            'shoes':['운동화', '부츠'],
+                            'outer':['코트', '패딩']}
     # 남성일 경우 여자 옷 제거
     if gender == '남성':
-        if '원피스' in clothes_kind.keys():
-            clothes_kind.pop('원피스', None)
-        if '치마' in clothes_kind['하의']:
-            clothes_kind['하의'] = [k for k in clothes_kind['하의'] if k != '치마']
+        if 'one' in clothes_kind.keys():
+            clothes_kind.pop('one', None)
+        if '치마' in clothes_kind['bottom']:
+            clothes_kind['bottom'] = [k for k in clothes_kind['bottom'] if k != '치마']
     
-    # 옷 선택지에 '원피스'가 있고 옷장에 '원피스'가 있을 경우, 원피스와 상하의 중에 random sample
-    if '원피스' in clothes_kind.keys():
-        if '원피스' in closet.category.values:
-            choice = random.sample(['원피스', '상하의'], 1)[0]
+    # 옷 선택지에 '원피스'가 있고 옷장에 '원피스'가 있을 경우, 원피스와 topbottom 중에 random sample
+    if 'one' in clothes_kind.keys():
+        if 'one' in closet.category.values:
+            choice = random.sample(['one', 'topbottom'], 1)[0]
         else:
-            choice = ['상하의']
+            choice = ['topbottom']
     else:
-        choice = ['상하의']
+        choice = ['topbottom']
 
     # 위의 선택에 따라 선택지 삭제
     kind = list(clothes_kind.keys())
-    if choice == '원피스':
-        kind.remove('상의')
-        kind.remove('하의')
+    if choice == 'one':
+        kind.remove('top')
+        kind.remove('bottom')
     else:
-        if '원피스' in kind:
-            kind.remove('원피스')
+        if 'one' in kind:
+            kind.remove('one')
                 
     # 옷 선택
     rot = True
@@ -105,7 +105,7 @@ def recommendation(temp, gender, closet):
             choices.extend(choice_pk)
         
         # 색상 배치
-        if choice == '원피스':
+        if choice == 'one':
             rot = False
         else:
             c1 = closet.get(pk=choices[0]).color # black
